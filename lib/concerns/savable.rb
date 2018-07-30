d3fb2c52af28d65f7ec96af020ffc276f40e0707
Micah Shute <micah.shute@gmail.com>
2018-07-30 15:24:36 +0000

    automatically backed up by learn

--- lib/concerns/savable.rb
+++ lib/concerns/savable.rb
@@ -5,20 +5,6 @@ module Savable
          self.class_variable_get("@@all")
      end
 
-     def find_by_name(name)
-         self.all.find{|i| i.name == name}
-     end
-
-     def create_or_update_from_data(data)
-         return if data[:name].nil?
-         found = self.find_by_name(data[:name])
-         item = found.nil? ? self.new : found
-         data.each do |k,v|
-             item.send("#{k}=", v)
-         end
-         item
-     end
-
      def clear
          self.all.clear
      end
