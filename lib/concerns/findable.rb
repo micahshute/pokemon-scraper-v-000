4a6790f2aaf2cc59b467a78d2bdf196289e9dbc4
Micah Shute <micah.shute@gmail.com>
2018-07-30 15:25:00 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,15 +1,17 @@
 module Findable
-  def find_by_name(name)
-      self.all.find{|i| i.name == name}
-  end
+  module ClassMethods
+    def find_by_name(name)
+        self.all.find{|i| i.name == name}
+    end
 
-  def create_or_update_from_data(data)
-      return if data[:name].nil?
-      found = self.find_by_name(data[:name])
-      item = found.nil? ? self.new : found
-      data.each do |k,v|
-          item.send("#{k}=", v)
-      end
-      item
+    def create_or_update_from_data(data)
+        return if data[:name].nil?
+        found = self.find_by_name(data[:name])
+        item = found.nil? ? self.new : found
+        data.each do |k,v|
+            item.send("#{k}=", v)
+        end
+        item
+    end
   end
 end
