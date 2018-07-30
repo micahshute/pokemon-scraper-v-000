17aff3472d4fd24ec18dbc0ce69962a3d910481d
Micah Shute <micah.shute@gmail.com>
2018-07-30 16:08:43 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -18,7 +18,7 @@ module Findable
 
     def find(id, db)
       data = db.execute("SELECT * FROM pokemon WHERE id = ?", [id])[0]
-      create_or_update_from_data({id: data[0], name: data[1], type: data[2], db: db})
+      create_or_update_from_data({id: data[0], name: data[1], type: data[2], db: db, hp: data[3]})
     end
   end
 end
